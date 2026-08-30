57a643ad79e60c24ee1adfd21d781013c138a7cd
Janardhan <janardhan59w@gmail.com>
2026-08-30 11:01:49 +0530

    Fix WhatsAppButton filename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./components/About";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsappButton";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
   return (
